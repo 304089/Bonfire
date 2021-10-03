c825f9dc522fb89f80a74303a9fe2c20a9db035b
304089 <9457imai2@gmail.com>
2021-10-03 16:03:52 +0000

    ブックマーク非同期

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,17 +1,15 @@
 class BookmarksController < ApplicationController
 
   def create
-    photo_post = PhotoPost.find(params[:photo_post_id])
-    bookmark = current_user.bookmarks.new(photo_post_id: photo_post.id)
+    @photo_post = PhotoPost.find(params[:photo_post_id])
+    bookmark = current_user.bookmarks.new(photo_post_id: @photo_post.id)
     bookmark.save
-    redirect_to photo_post_path(photo_post)
   end
 
   def destroy
-    photo_post = PhotoPost.find(params[:photo_post_id])
-    bookmark = current_user.bookmarks.find_by(photo_post_id: photo_post.id)
+    @photo_post = PhotoPost.find(params[:photo_post_id])
+    bookmark = current_user.bookmarks.find_by(photo_post_id: @photo_post.id)
     bookmark.destroy
-    redirect_to photo_post_path(photo_post)
   end
 
 end
