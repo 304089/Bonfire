33e808ce44db49e52a349f8ba8f7766407c67a9d
304089 <9457imai2@gmail.com>
2021-10-03 15:56:24 +0000

    いいね非同期化

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,17 +1,15 @@
 class FavoritesController < ApplicationController
 
   def create
-    photo_post = PhotoPost.find(params[:photo_post_id])
-    favorite = current_user.favorites.new(photo_post_id: photo_post.id)
+    @photo_post = PhotoPost.find(params[:photo_post_id])
+    favorite = current_user.favorites.new(photo_post_id: @photo_post.id)
     favorite.save
-    redirect_to photo_post_path(photo_post)
   end
 
   def destroy
-    photo_post = PhotoPost.find(params[:photo_post_id])
-    favorite = current_user.favorites.find_by(photo_post_id: photo_post.id)
+    @photo_post = PhotoPost.find(params[:photo_post_id])
+    favorite = current_user.favorites.find_by(photo_post_id: @photo_post.id)
     favorite.destroy
-    redirect_to photo_post_path(photo_post)
   end
 
 end
