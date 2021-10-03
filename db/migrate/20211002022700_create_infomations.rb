8383822e3bd34e78e6b725db3bd978c96eb8a00c
304089 <9457imai2@gmail.com>
2021-10-03 14:15:04 +0000

    main

--- db/migrate/20211002022700_create_infomations.rb
+++ db/migrate/20211002022700_create_infomations.rb
@@ -2,7 +2,6 @@ class CreateInfomations < ActiveRecord::Migration[5.2]
   def change
     create_table :infomations do |t|
       t.string :email, null: false
-      t.string :genre, null: false,default: 0
       t.text :content, null: false
       t.string :status,null: false,default: 0
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -71,7 +71,6 @@ ActiveRecord::Schema.define(version: 2021_10_03_085643) do
 
   create_table "infomations", force: :cascade do |t|
     t.string "email", null: false
-    t.string "genre", default: "0", null: false
     t.text "content", null: false
     t.string "status", default: "0", null: false
     t.datetime "created_at", null: false
