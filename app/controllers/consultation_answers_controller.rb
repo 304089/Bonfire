f1ad456282c1aac6bc85ae5e55cb33a3b8ff1706
304089 <9457imai2@gmail.com>
2021-10-04 09:34:05 +0000

    相談回答非同期か

--- app/controllers/consultation_answers_controller.rb
+++ app/controllers/consultation_answers_controller.rb
@@ -1,15 +1,21 @@
 class ConsultationAnswersController < ApplicationController
 
   def create
-    consultation = Consultation.find(params[:consultation_id])
+    @consultation = Consultation.find(params[:consultation_id])
+    @consultation_answers = ConsultationAnswer.where(consultation_id: @consultation.id)
     @consultation_answer = ConsultationAnswer.new(consultation_answer_params)
     @consultation_answer.user_id = current_user.id
-    @consultation_answer.consultation_id = consultation.id
-    @consultation_answer.save!
-    redirect_to request.referer
+    @consultation_answer.consultation_id = @consultation.id
+    @consultation_answer.save
+    render :answers
   end
 
   def destroy
+    @consultation = Consultation.find(params[:consultation_id])
+    @consultation_answers = ConsultationAnswer.where(consultation_id: @consultation.id)
+    @consultation_answer = ConsultationAnswer.find(params[:id])
+    @consultation_answer.destroy
+    render :answers
   end
 
   private
