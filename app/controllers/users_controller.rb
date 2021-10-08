be70633944a4eb5173f82e7ea2fab274ab6113e7
304089 <9457imai2@gmail.com>
2021-10-08 14:32:29 +0000

    マイページ写真一覧レイアウト完成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @photo_posts = PhotoPost.where(user_id: @user.id).order(id: "DESC")
+    @photo_posts = PhotoPost.where(user_id: @user.id).order(id: "DESC").limit(9)
   end
 
   def index
